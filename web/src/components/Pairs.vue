458793fff9ff1d2a1294122712e4c4ac2c205362
Андрей Еберзин <andrey.eberzin@gmail.com>
2021-03-14 16:34:17 +0500

    data

--- web/src/components/Pairs.vue
+++ web/src/components/Pairs.vue
@@ -45,9 +45,9 @@ export default class Pairs extends Vue {
   get tables() {
     let current = 0;
     return this.column.split(',').map((column: string) => {
-      this.pairs.slice(current, parseInt(column));
+      const result = this.pairs.slice(current, parseInt(column));
       current = parseInt(column);
-      console.log(current, column);
+      return result;
     });
     // return [this.pairs.slice(0, 28), this.pairs.slice(28, 57)]
   }
